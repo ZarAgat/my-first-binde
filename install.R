1d8f57521b850e7edd4ddb950bdca1127b891954
Agata Zaremba <56956530+ZarAgat@users.noreply.github.com>
2021-06-14 15:16:16 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,4 @@
 install.packages(c("readr","BiocManager","devtools"), dependencies=T )
 devtools::install_deps()
-holepunch::write_install(c("readr","BiocManager"), dependencies=T )
-
 BiocManager::install("DESeq2")
+holepunch::write_install()
